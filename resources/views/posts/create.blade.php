f1d24dc0e118bf34f00d5d1367286bf3e32d83fa
reynald_tolentino <reynald.tolentino@gmail.com>
2016-06-23 15:21:28 +0800

    Laravel Blog Study Part 12.5 - Javascript Form Validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', '| Create New Post')
 
+@section('stylesheets')
+    {!! Html::style('css/parsley.css') !!}
+@endsection
+
 @section('content')
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -9,16 +13,20 @@
             <hr>
 
             <!-- the following form will automatically create a csrf protection -->
-            {!! Form::open(array('route' => 'posts.store')) !!}
+            {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
                 {{ Form::label('title', 'Title:') }}
-                {{ Form::text('title', null, array('class' => 'form-control')) }}
+                {{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
 
                 {{ Form::label('title', 'Post Body:') }}
-                {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+                {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
 
                 {{ Form::submit('Create Post', array('class'=> 'btn btn-success btn-lg btn-block', 'style' =>'margin-top:20px')) }}
             {!! Form::close() !!}
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    {!! Html::script('js/parsley.min.js') !!}
 @endsection
